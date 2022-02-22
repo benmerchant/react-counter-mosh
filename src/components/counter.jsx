9a90f7ec26176a1b8d718c78c0324e3281a1669b
Ben Merchant <benjaminmerchant@gmail.com>
2022-02-22 14:01:29 -0500

    clunky solution to binding eventhandlers

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -6,23 +6,38 @@ class Counter extends Component {
     tags: [],
   };
 
-  renderTags() {
-    if (this.state.tags.length === 0) return <p>There are no tags</p>;
-    return (
-      <ul>
-        {this.state.tags.map((tag) => (
-          <li key={tag}>{tag}</li>
-        ))}
-      </ul>
-    );
+  // renderTags() {
+  //   if (this.state.tags.length === 0) return <p>There are no tags</p>;
+  //   return (
+  //     <ul>
+  //       {this.state.tags.map((tag) => (
+  //         <li key={tag}>{tag}</li>
+  //       ))}
+  //     </ul>
+  //   );
+  // }
+
+  constructor() {
+    super();
+    this.handleIncrement = this.handleIncrement.bind(this);
+  }
+
+  handleIncrement() {
+    console.log('Increment Clicked', this.state.count);
   }
 
   render() {
     return (
       <React.Fragment>
         <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
-        <button className='btn btn-secondary btn-sm'>Increment</button>
-        {this.renderTags()}
+        <button
+          onClick={this.handleIncrement}
+          className='btn btn-secondary btn-sm'
+        >
+          Increment
+        </button>
+        {/* {this.state.tags.length === 0 && 'Please create a new tag!'}
+        {this.renderTags()} */}
       </React.Fragment>
     );
   }
