a1d4856d244b2fee1628cc060e8a53231e1279b4
Ben Merchant <benjaminmerchant@gmail.com>
2022-02-22 20:21:07 -0500

    SFC works now.. didn't watch the video enough

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,25 +1,20 @@
 import React, { Component } from 'react';
 
-// Stateless Functional Componenet
-// cant get to work ^^^
+// Stateless Functional Component
+const Navbar = (props) => {
+  return (
+    <nav className='navbar navbar-expand-md navbar-dark bg-dark mb-4'>
+      <div className='container-fluid'>
+        <a className='navbar-brand' href='#'>
+          Ben Merchant React App #000
+        </a>
 
-class Navbar extends Component {
-  state = {};
-  render() {
-    return (
-      <nav className='navbar navbar-expand-md navbar-dark bg-dark mb-4'>
-        <div className='container-fluid'>
-          <a className='navbar-brand' href='#'>
-            Ben Merchant React App #000
-          </a>
-
-          <span className='badge badge-pill bg-secondary'>
-            {this.props.totalCounters}
-          </span>
-        </div>
-      </nav>
-    );
-  }
-}
+        <span className='badge badge-pill bg-secondary'>
+          {props.totalCounters}
+        </span>
+      </div>
+    </nav>
+  );
+};
 
 export default Navbar;
